324db1d74aa30f813b8dbb8e80158cdd48ff831e
bhargavi201994 <145483318+bhargavi201994@users.noreply.github.com>
2023-11-23 12:00:21 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 var request = require('supertest');
 var app = require('../index.js');
+
 describe('GET /will', function() {
     it('respond with Hello World', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+        request(app)
+            .get('/will')
+            .expect(200)  // Expecting a 200 OK response
+            .expect({ response: 'Hello World' })  // Expecting a JSON response
+            .end(done);  // Call done when the assertions are complete
     });
 });
